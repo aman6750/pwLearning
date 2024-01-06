43e951cb5ef33ba0197d977338ec1a1db8d82898
Aman <amanyadav6750@gmail.com>
2024-01-07 00:53:01 +0530

    2

---
+++ live session1/index.js
@@ -0,0 +1,21 @@
+// var student = {
+//     name: "aman",
+//     age:25,
+//     gender:"male",
+//     city : "lucknow",
+//     hobbies: "coding",
+//     smoking : false
+// };
+
+// console.log(student["name"]);
+
+
+// var text = "       Hello World!        ";
+// var result = text.trim();
+// console.log(result);
+
+
+// var str = "Masai School";
+// var output = str.split(" ");
+// console.log(output);
+
